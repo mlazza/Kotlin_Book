76e8645f78ea59a81b629b8f9b128826592ff776
mlazza <mlazzarotti@gmail.com>
2021-05-13 18:37:30 -0300

    updating

--- src/Game.kt
+++ src/Game.kt
@@ -1,6 +1,6 @@
 fun main() {
     val name = "Madrigal"
-    val healthPoints = 75
+    val healthPoints = 15
     val isBlessed = true
     val isImmortal = false
 
@@ -10,20 +10,16 @@ fun main() {
     val auraColor = if (auraVisible) "GREEN" else "NONE"
     println(auraColor)
 
-    val healthStatus = if(healthPoints == 100){
-        "is in excellent condition!"
-    } else if (healthPoints in 90..99) {
-        "has a few scratches."
-    } else if (healthPoints in 75..89) {
-        if (isBlessed) {
+    val healthStatus = when(healthPoints){
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
             "has some minor wounds but is healing quite quickly!"
-        } else {
-            "has some minor wounds."
-        }
-    } else if(healthPoints in 15..74){
-        "looks pretty hurt."
-    } else {
-        "is in awful condition!"
+            } else {
+                "has some minor wounds."
+            }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
     }
 
     // Player Status
